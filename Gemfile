01da89fd39919aeb4953701b9da58b7b58dfddf7
Maha Mahmood <>
2020-09-28 14:44:19 -0400

    bundled jwt and dotenv. required dotenv in application.rb in config. add a create token and payload methods in user controller. server can send a token through to client when login.

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'faker'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+gem 'jwt'
+gem 'dotenv-rails'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   def login
     user = User.find_by(username: params[:user][:username])
     if user && user.authenticate(params[:user][:password])
-      render json: { status: 200, user: user }
+      token = create_token(user.id, user.username)
+      render json: { status: 200, token: token, user: user }
     else
       render json: { status: 401, message: "Unauthorized" }
     end
@@ -49,6 +50,22 @@ class UsersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def create_token(id, username)
+      JWT.encode(payload(id, username), ENV['JWT_SECRET'], 'HS256')
+    end
+    
+    def payload(id, username)
+      {
+        exp: (Time.now + 30.minutes).to_i,
+        iat: Time.now.to_i,
+        iss: ENV['JWT_ISSUER'],
+        user: {
+          id: id,
+          username: username
+        }
+      }
+    end
+
     def set_user
       @user = User.find(params[:id])
     end
