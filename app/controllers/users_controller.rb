f0f70fea7d66c9422d319dabd71b7ba746f71565
Maha Mahmood <>
2020-10-01 13:24:23 -0400

    made one edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
-    # render json: @user.to_json(include: :courses)
+    # render json: @user.to_json(include: :courses) 
     render json: get_current_user.to_json(include: :courses)
   end
 
